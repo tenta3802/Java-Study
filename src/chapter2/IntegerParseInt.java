37275473cad5bfb92b0af39466efe0c29bff6226
tenta <eunoia3802@gmail.com>
2022-08-03 19:46:24 +0900

    Integer.parseInt()

---
+++ src/chapter2/IntegerParseInt.java
@@ -0,0 +1,19 @@
+package chapter2;
+
+import java.util.Scanner;
+
+public class IntegerParseInt {
+	
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		
+		int num = Integer.parseInt(input); // String -> Integer 값으로 변환
+		
+		System.out.println(input.getClass().getName()); //input 타입 확인
+		System.out.println(num+3); // 변환 후 테스트
+
+	}
+
+}
