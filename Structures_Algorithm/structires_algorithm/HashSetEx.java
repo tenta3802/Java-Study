94dc5abe33b011509cd54416791e5a78fd269d5c
tenta <eunoia3802@gmail.com>
2022-08-28 23:48:34 +0900

    HashSet 구현 예제

--- Structures_Algorithm/structires_algorithm/HashSetEx.java
+++ Structures_Algorithm/structires_algorithm/HashSetEx.java
@@ -1,31 +1,31 @@
 package structires_algorithm;
 
-import java.util.HashSet;
+import java.util.HashSet; 
 import java.util.Iterator;
 public class HashSetEx {
-//
-//	public static void main(String[] args) {
-//		
-//		HashSet<Integer> set = new HashSet<>();
-//		HashSet<Integer> set2 = new HashSet<>();
-//
-//		set.add(1); // 값 추가
-//		set.remove(1); // 값이 1인 데이터 삭제
-//		set.removeAll(set2); // set의 데이터 중 set2에 들어있는 데이터를 모두 삭제
-//		set.retainAll(set2); // set의 데이터 중 set2에 들어있지 않은 데이터를 모두 삭제
-//		set.clear(); // 모든 데이터 삭제
-//		set.size(); // 크기 반환
-//		set.contains(1); // 값 1이 있으면 true, 없으면 false
-//
-//		// 값 출력
-//		// 방법 1: get 메소드가 없으므로 원소에 접근하려면 이터레이터 사용
-//		Iterator iter = set.iterator();
-//		while (iter.hasNext())
-//			System.out.println(iter.next());
-//
-//		// 방법 2: for-each문으로 원소에 접근
-//		for (String item: set)
-//			System.out.println(item);
-//	}
-//
+
+	public static void main(String[] args) {
+		
+		HashSet<Integer> set = new HashSet<>();
+		HashSet<Integer> set2 = new HashSet<>();
+
+		set.add(1); // 값 추가
+		set.remove(1); // 값이 1인 데이터 삭제
+		set.removeAll(set2); // set의 데이터 중 set2에 들어있는 데이터를 모두 삭제
+		set.retainAll(set2); // set의 데이터 중 set2에 들어있지 않은 데이터를 모두 삭제
+		set.clear(); // 모든 데이터 삭제
+		set.size(); // 크기 반환
+		set.contains(1); // 값 1이 있으면 true, 없으면 false
+
+		// 값 출력
+		// 방법 1: get 메소드가 없으므로 원소에 접근하려면 이터레이터 사용
+		Iterator iter = set.iterator();
+		while (iter.hasNext())
+			System.out.println(iter.next());
+
+		// 방법 2: for-each문으로 원소에 접근
+		for (Integer item: set)
+			System.out.println(item);
+	}
+
 }
