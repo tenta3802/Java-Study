7ce3f8009a8bc7086db936f88a6eb7b68b59773d
tenta <eunoia3802@gmail.com>
2022-09-01 21:50:33 +0900

    Baekjoon 1919

--- Baekjoon/baekjoon/Main_1919.java
+++ Baekjoon/baekjoon/Main_1919.java
@@ -10,7 +10,6 @@ import java.util.Scanner;
 public class Main_1919 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
 		String a = sc.next();
 		String b = sc.next();
 		
